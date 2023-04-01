3f75f4823e401c5f2b9860bfab72047eb680504c
Abror <komalov@me.com>
2023-04-01 22:46:42 +0500

    removed serverfeatures

--- src/utils/createUser.ts
+++ src/utils/createUser.ts
@@ -1,26 +1,26 @@
-import { connectToDatabase } from './modgoDB';
+// import { connectToDatabase } from './modgoDB';
 
-export async function createUser(user: {
-    id: string;
-    email: string;
-    firstName: string;
-    lastName: string;
-    password: string;
-}) {
-    const db = await connectToDatabase();
+// export async function createUser(user: {
+//     id: string;
+//     email: string;
+//     firstName: string;
+//     lastName: string;
+//     password: string;
+// }) {
+//     const db = await connectToDatabase();
 
-    // Insert the user into the "users" collection
-    const result = await db.collection('users').insertOne(user);
+//     // Insert the user into the "users" collection
+//     const result = await db.collection('users').insertOne(user);
 
-    console.log(`Created a new user with the following ID: ${result.insertedId}`);
-}
+//     console.log(`Created a new user with the following ID: ${result.insertedId}`);
+// }
 
-const newUser = {
-    id: '123456789',
-    firstName: 'John Doe',
-    lastName: 'John Doe',
-    email: 'john.doe@example.com',
-    password: 'password123',
-};
+// const newUser = {
+//     id: '123456789',
+//     firstName: 'John Doe',
+//     lastName: 'John Doe',
+//     email: 'john.doe@example.com',
+//     password: 'password123',
+// };
 
-createUser(newUser);
+// createUser(newUser);
--- src/utils/modgoDB.ts
+++ src/utils/modgoDB.ts
@@ -1,24 +1,24 @@
-import { MongoClient, Db } from 'mongodb';
+// import { MongoClient, Db } from 'mongodb';
 
-// Connection URI for your MongoDB database
-const uri =
-    'mongodb+srv://<komalov>:<abrorium72>@<komalov>.mongodb.net/<asos_DB>?retryWrites=true&w=majority';
+// // Connection URI for your MongoDB database
+// const uri =
+//     'mongodb+srv://<komalov>:<abrorium72>@<komalov>.mongodb.net/<asos_DB>?retryWrites=true&w=majority';
 
-// Create a new MongoClient
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// // Create a new MongoClient
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function connectToDatabase(): Promise<Db> {
-    try {
-        // Connect to the MongoDB database
-        await client.connect();
-        console.log('Connected to the database');
+// async function connectToDatabase(): Promise<Db> {
+//     try {
+//         // Connect to the MongoDB database
+//         await client.connect();
+//         console.log('Connected to the database');
 
-        // Return a reference to the database object
-        return client.db('<database>');
-    } catch (error) {
-        console.error('Error connecting to the database', error);
-        throw error;
-    }
-}
+//         // Return a reference to the database object
+//         return client.db('<database>');
+//     } catch (error) {
+//         console.error('Error connecting to the database', error);
+//         throw error;
+//     }
+// }
 
-export { connectToDatabase };
+// export { connectToDatabase };
